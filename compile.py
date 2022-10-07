07493cf3f4c2a5ac628535d04cfaf7643ea700e1
kavidey <kaviasher@gmail.com>
2022-10-07 09:41:46 -0700

    First simple version of the compiler working

--- compile.py
+++ compile.py
@@ -2,6 +2,8 @@ import argparse
 import pycparser
 from pycparser import parse_file
 
+import re
+
 def check_ast(ast):
     # TODO: check ast
     # This includes checking for:
@@ -32,8 +34,30 @@ if __name__ == "__main__":
     # Make sure the AST contains valid code
     check_ast(ast)
 
+    code = []
+
     for child in ast.ext:
         if type(child) == pycparser.c_ast.FuncDef:
             if child.decl.name == "main":
                 for stmt in child.body.block_items:
-                    print(stmt)
+                    # If the user is declaring a new variable
+                    if type(stmt) == pycparser.c_ast.Decl:
+                        assert stmt.type.type.names[0] == "int", "Only ints are supported"
+                        assert re.match("r\d", stmt.name), "Variables names must be in the form rX"
+                        assert 1 <= int(stmt.name[1:]) <= 12, "Variables must be named r1, r2, ..., r12"
+                        assert stmt.init != None, "Variable declarations must have an initial value"
+                        assert -128 <= int(stmt.init.value) <= 127, "Variable initial values must be between -128 and 127"
+
+                        code += [f"setn {stmt.name} {stmt.init.value}"]
+                    
+                    elif type(stmt) == pycparser.c_ast.FuncCall:
+                        assert stmt.name.name == "printf", "Only printf is supported"
+                        assert stmt.args.exprs[0].value == '"%d\\n"', "Only printf(\"%d\\n\", ...) is supported"
+
+                        code += [f"write {stmt.args.exprs[1].name}"]
+    
+    output = ""
+    for i,line in enumerate(code):
+        output += f"{i:03d} {line} \n"
+    output += f"{len(code):03d} halt"
+    print(output)
