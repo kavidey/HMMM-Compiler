36d4f5df52006587eb434731f1e38a9804ded5d0
kavidey <kaviasher@gmail.com>
2022-10-09 15:16:15 -0700

    Add documentation and fix bugs

--- compile.py
+++ compile.py
@@ -17,6 +17,14 @@ def check_ast(ast):
     pass
 
 def find_used_registers(node: pycparser.c_ast.Node) -> set[HmmmRegister]:
+    """Gets the set of all registers used in the given node (and its children)
+
+    Args:
+        node (pycparser.c_ast.Node) -- The node to search
+    
+    Returns:
+        set[HmmmRegister] -- A set of all registers used in the given node and its children
+    """
     if isinstance(node, pycparser.c_ast.ID):
         return {HmmmRegister(node.name)}
     elif isinstance(node, pycparser.c_ast.Constant):
@@ -31,22 +39,38 @@ def find_used_registers(node: pycparser.c_ast.Node) -> set[HmmmRegister]:
         raise NotImplementedError(f"Cannot find used registers for {type(node)}")
 
 def find_unused_registers(set_of_registers: set[HmmmRegister]) -> set[HmmmRegister]:
+    """ Finds all registers that are not in the given set of registers
+
+    Args:
+        set_of_registers (set[HmmmRegister]) -- The set of registers to check
+    
+    Returns:
+        set[HmmmRegister] -- A set of all registers that are not in the given set of registers
+    """
     return {register for register in HmmmRegister if register not in set_of_registers}
 
 def parse_binary_op(node: pycparser.c_ast.BinaryOp, program: HmmmProgram) -> None:
+    """Parses a binary operation and adds the appropriate instructions to the given program
+
+    Recursively walks through the tree of the given node and turns binary operations (+, -, *, /) into Hmmm instructions.
+    
+    Args:
+        node (pycparser.c_ast.BinaryOp) -- The node to parse
+        program (HmmmProgram) -- The program to add instructions to
+    """
+    used_registers = find_used_registers(node)
+    assert len(used_registers) < 11, "Cannot use more than 10 variables in a single expression"
+    unused_registers = find_unused_registers(used_registers.union({HmmmRegister.R0, HmmmRegister.R13, HmmmRegister.R14, HmmmRegister.R15}))
+    left_register = unused_registers.pop()
+    right_register = unused_registers.pop()
+
     ### Left Side ###
     # If left is none, then the result of the left side is in R13
     # if left is an int, then the result of the left side is a constant
     # if left is a register, then the result of the left side is in that register
     # Whatever the left side is, we want to move it into an unused register
-
-    used_registers = find_used_registers(node)
-    assert len(used_registers) < 11, "Cannot use more than 10 variables in a single expression"
-    unused_registers = find_unused_registers(used_registers.union({HmmmRegister.R13, HmmmRegister.R14, HmmmRegister.R15}))
-    left_register = unused_registers.pop()
-    right_register = unused_registers.pop()
-
     left = None
+    program.add_comment("Left Side")
     program.add_instruction(generate_instruction("pushr", left_register))
     if isinstance(node.left, pycparser.c_ast.ID):
         left = HmmmRegister(node.left.name)
@@ -56,13 +80,14 @@ def parse_binary_op(node: pycparser.c_ast.BinaryOp, program: HmmmProgram) -> Non
         program.add_instruction(generate_instruction("setn", left_register, left))
     elif isinstance(node.left, pycparser.c_ast.BinaryOp):
         program.add_instruction(generate_instruction("pushr", HmmmRegister.R13))
-        program.add_instruction(generate_instruction("copy", left_register, HmmmRegister.R13))
         parse_binary_op(node.left, program)
+        program.add_instruction(generate_instruction("copy", left_register, HmmmRegister.R13))
         program.add_instruction(generate_instruction("popr", HmmmRegister.R13))
     
     ### Right Side ###
     # The logic here is the same as the left side
     right = None
+    program.add_comment("Right Side")
     program.add_instruction(generate_instruction("pushr", right_register))
     if isinstance(node.right, pycparser.c_ast.ID):
         right = HmmmRegister(node.right.name)
@@ -72,22 +97,23 @@ def parse_binary_op(node: pycparser.c_ast.BinaryOp, program: HmmmProgram) -> Non
         program.add_instruction(generate_instruction("setn", right_register, right))
     elif isinstance(node.right, pycparser.c_ast.BinaryOp):
         program.add_instruction(generate_instruction("pushr", HmmmRegister.R13))
-        program.add_instruction(generate_instruction("copy", right_register, HmmmRegister.R13))
         parse_binary_op(node.right, program)
+        program.add_instruction(generate_instruction("copy", right_register, HmmmRegister.R13))
         program.add_instruction(generate_instruction("popr", HmmmRegister.R13))
-    
+
     ### Operation ###
     if node.op == "+":
-        program.add_instruction(generate_instruction("add", left_register, right_register, HmmmRegister.R13))
+        program.add_instruction(generate_instruction("add", HmmmRegister.R13, left_register, right_register))
     elif node.op == "-":
-        program.add_instruction(generate_instruction("sub", left_register, right_register, HmmmRegister.R13))
+        program.add_instruction(generate_instruction("sub", HmmmRegister.R13, left_register, right_register))
     elif node.op == "*":
-        program.add_instruction(generate_instruction("mul", left_register, right_register, HmmmRegister.R13))
+        program.add_instruction(generate_instruction("mul", HmmmRegister.R13, left_register, right_register))
     elif node.op == "/":
-        program.add_instruction(generate_instruction("div", left_register, right_register, HmmmRegister.R13))
+        program.add_instruction(generate_instruction("div", HmmmRegister.R13, left_register, right_register))
     elif node.op == "%":
-        program.add_instruction(generate_instruction("mod", left_register, right_register, HmmmRegister.R13))
+        program.add_instruction(generate_instruction("mod", HmmmRegister.R13, left_register, right_register))
     
+    # Pop both registers that we used to store the left and right side of this operation
     program.add_instruction(generate_instruction("popr", right_register))
     program.add_instruction(generate_instruction("popr", left_register))
 
@@ -138,7 +164,7 @@ if __name__ == "__main__":
 
                         program.add_instruction(generate_instruction("write", HmmmRegister(stmt.args.exprs[1].name)))
     
-    program.add_stack_pointer_code()
     program.add_instruction(generate_instruction("halt"))
+    program.add_stack_pointer_code()
     program.assign_line_numbers()
     print(program)
